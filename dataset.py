edec22fe93aa6620115aa76e67d49f945134010f
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-08-24 16:21:50 +0100

    fixed bugs

--- dataset.py
+++ dataset.py
@@ -141,7 +141,7 @@ def collate_fn2(data): #, history_word_num=60
         indication_prompts[i,:len(tokens)] = tokens
         
     indication_prompts = torch.tensor(indication_prompts).type(torch.LongTensor)
-    probs = torch.tensor(probs) 
+    probs = torch.tensor(probs).type(torch.LongTensor)
     targets = torch.tensor(targets).type(torch.LongTensor)
     
     #print(type(prompt), type(label), type(target), type(prob))
