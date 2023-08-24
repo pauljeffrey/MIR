eeede9eaa86b3e7b0a3c1df5df27b9e947834389
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-08-24 15:36:37 +0100

    fixed bugs

--- utils.py
+++ utils.py
@@ -24,9 +24,10 @@ def create_masks(inputs):
 
 
 def create_padding_mask(inputs):
+    """ False for non-padding element otherwise, True"""
     # if type(inputs) == np._NdArraySubClass:
     #     print("yes")
-    mask = (inputs == 0)#.unsqueeze(1).unsqueeze(2)
+    mask = torch.tensor((inputs == 0))#.unsqueeze(1).unsqueeze(2)
     mask = mask.float().masked_fill(mask == 1, float('-inf'))#.masked_fill(mask == 1, float(0.0))
     return mask
 
@@ -37,23 +38,25 @@ if __name__ == '__main__':
     #print(create_causal_masks(inputs))
     # print(generate_square_subsequent_mask(5))
     # print()
-    print(create_padding_mask(inputs).shape)
+    mask = create_padding_mask(inputs)
     # # print()
     
     # print(inputs)
     
-    q = torch.randn( 5,3, 10) # source sequence length 3, batch size 1, embedding size 10
+    q = torch.randn( 3,5, 10) # source sequence length 3, batch size 1, embedding size 10
     w= torch.randn(1,3)
     attn = nn.MultiheadAttention(10, 1, batch_first=True) # embedding size 10, one head
-    attn(q, q, q) # self attention
+    #attn(q, q, q) # self attention
     
-    def src_mask(sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        return mask
-    out = attn(q, q, q, attn_mask=src_mask(3))
+    # def src_mask(sz):
+    #     mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+    #     mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    #     return mask
+    
+    out = attn(q, q, q, key_padding_mask= mask, attn_mask=src_mask(5))#, 
     print(out[1][0], out[0][0])
     print(out[1].shape, out[0].shape) # attention output weights
-    print(src_mask(3))
+    print(src_mask(5))
+    print(mask)
     
     
