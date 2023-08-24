6b1926f15828d16ccf9dc73d2fed9734028e2cda
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-08-24 21:49:29 +0100

    fixed bugs

--- loss.py
+++ loss.py
@@ -26,8 +26,8 @@ class CustomLoss(nn.Module):
         # print("Outpoutshape: ", y2_pred.shape, y2_true.shape)
         #y2_pred = y2_pred[y2_mask].permute(0,2,1) # shape == (batch_size, n_classes, seq_len)
         #print("output shape: ", y2_pred.shape, y2_true[y2_mask])
-        sparse_loss = self.cross_entropy(y2_pred[y2_mask], y2_true[y2_mask])
-        bce_loss = self.bce(y1_pred[y1_mask], y1_true[y1_mask])
+        sparse_loss = torch.sqrt(self.cross_entropy(y2_pred[y2_mask], y2_true[y2_mask]))
+        bce_loss = torch.sqrt(self.bce(y1_pred[y1_mask], y1_true[y1_mask]))
         
         print("sparse_loss: ", sparse_loss)
         print("stop loss: ", bce_loss)
@@ -50,8 +50,8 @@ class CustomBCELoss(nn.Module):
         label_true = label_true.to(torch.float32)
         label_pred  = label_pred.to(torch.float32)
         #print(label_pred, label_true)
-        label_loss = self.bce(label_pred[label_mask], label_true[label_mask])
-        print("label_loss: ", label_loss)
+        label_loss = torch.sqrt(self.bce(label_pred[label_mask], label_true[label_mask]))
+        #print("label_loss: ", label_loss)
         return label_loss
 
 
