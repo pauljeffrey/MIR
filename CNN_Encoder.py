1214f106a059b3be5ffa7763717ba44b5e8f4b5b
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-08-30 00:16:36 +0100

    fix bugs

--- CNN_Encoder.py
+++ CNN_Encoder.py
@@ -135,8 +135,13 @@ class MLC(nn.Module):
 
     def forward(self, avg_features):
         avg_features = self.linear(avg_features)
-        print("average features : ", avg_features.shape)
+        print("average features : ", avg_features.shape, torch.min(avg_features), torch.max(avg_features))
+        if torch.any(torch.isnan(avg_features)):
+            print("avg features is nan")
         output = self.classifier(avg_features)
+        print("output after classifying average: ", output.shape, torch.min(output), torch.max(output))
+        if torch.any(torch.isnan(output)):
+            print("average features is nan..")
         tags = self.sigmoid(output)
         #semantic_features = self.embed(torch.topk(tags, self.k)[1])
         return tags #, semantic_features
