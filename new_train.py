f1c750a17c188f10f2343864c432b7536124dcc2
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-08-29 19:20:25 +0100

    fixed bugs

--- new_train.py
+++ new_train.py
@@ -120,19 +120,32 @@ def create_optimizer(cfg, model):
 def evaluate(model, accelerator, eval_loader, custom_loss, bce_loss):
     model.eval()
     device = accelerator.device
-    loss = 0
+    #loss = 0
+    
     with torch.no_grad():
         eval_losses = []
         eval_stop_losses = []
         eval_bce_losses = []
+        
         for _ , (encoded_images,indication_prompt, labels, true_stop_probs, reports) in enumerate(eval_loader):        
+            #n_sentences  = reports.shape[1]
+            
             n_sentences  = reports.shape[1]
+            # print('Length of sentences: ', n_sentences)
+            # print("Inputs shape: ", encoded_images.shape, indication_prompt.shape, labels.shape, true_stop_probs.shape, reports.shape)
+            # print("True probs: ",true_stop_probs)
             
             encoder_pad_mask = create_padding_mask(indication_prompt).to(device)
             #print("Mem shape: ", indication_prompt.shape, "mask shape: ", encoder_pad_mask.shape)
             #encoder_causal_mask = src_mask(indication_prompt.shape[1])
             
             encoded_images , tags = model.encoder(encoded_images)#.type(torch.cuda.HalfTensor))
+            
+            if torch.any(torch.isinf(encoded_images)) or torch.any(torch.isnan(encoded_images)):
+                print("Encoded Images is nan")
+            
+            # if torch.any(torch.isinf(tags)) or torch.any(torch.isnan(tags)):
+            #     print("Tags are nan")
             #print("Encoded Images: ", encoded_images.shape)
             bs , n_channels = encoded_images.shape[0], encoded_images.shape[1]
             
@@ -141,8 +154,12 @@ def evaluate(model, accelerator, eval_loader, custom_loss, bce_loss):
                 
             if model.history_encoder is not None:
                 indication_prompt = model.decoder.embed_layer(indication_prompt)
-                indication_prompt = model.history_encoder(indication_prompt, mask=encoder_pad_mask.type(indication_prompt.dtype))
+                # if torch.any(torch.isinf(indication_prompt)) or torch.any(torch.isnan(indication_prompt)):
+                #     print("Encoding by decoder embedding layer is nan")
                     
+                indication_prompt = model.history_encoder(indication_prompt, mask=encoder_pad_mask.type(indication_prompt.dtype))
+                if torch.any(torch.isinf(indication_prompt)) or torch.any(torch.isnan(indication_prompt)):
+                    print("Encoding by History encoder is nan")  
 
             # # compute mask, confirm the first part.
             # mem_mask = torch.cat([torch.zeros((encoded_images.shape[0], encoded_images.shape[1])), encoder_pad_mask], dim=-1)
@@ -157,8 +174,16 @@ def evaluate(model, accelerator, eval_loader, custom_loss, bce_loss):
             #lstm_init = True
             #print(hn.shape, cn.shape)
             #output = model(encoded_images, reports[:, :-1])  # [batch_size, seq_len - 1, vocab_size]
+            if torch.any(torch.isinf(prev_hidden)) or torch.any(torch.isnan(prev_hidden)):
+                    print("Initial Prev hidden, hn, cn is nan")
+                    
             for i in range(n_sentences):
+                print(f"This loop is for the number {i} sentence.")
                 
+                for name , each in model.named_parameters():
+                    if torch.any(torch.isnan(each)):
+                        print(name, " layer has nan values in it..")
+                    
                 if model.co_attention:
                     if model.history_encoder is not None:
                         #print("Encoded Images before attentino: ", encoded_images.shape)
@@ -172,13 +197,24 @@ def evaluate(model, accelerator, eval_loader, custom_loss, bce_loss):
                     else:
                         context_vector, att_wts = model.attention(prev_hidden, encoded_images)
 
+                # if torch.any(torch.isinf(context_vector)) or torch.any(torch.isnan(context_vector)):
+                #     print("Context Vector is nan")
+                    
                 # Generate Topic Vector
                 #print("Context and hidden shape before entering lstm: ", context_vector.shape, prev_hidden.shape)
                 prev_hidden, pred_stop_probs, (hn, cn) = model.sent_lstm(context_vector, prev_hidden, (hn, cn))  # [batch_size, d_model]
                 #lstm_init= False
-                
+                if torch.any(torch.isinf(prev_hidden)) or torch.any(torch.isnan(prev_hidden)):
+                    print("New prev hidden is nan")
+                    
+                if torch.any(torch.isinf(hn)) or torch.any(torch.isnan(hn)):
+                    print("hn , cn are nan")
+                    
+                if torch.any(torch.isinf(pred_stop_probs)) or torch.any(torch.isnan(pred_stop_probs)):
+                    print("Pred_stop_probs is nan")
                 # Decode reports
                 tgt = reports[:,i, :-1]  # Remove last token from reports
+                #print("Target indices: ", tgt)
                 #print('Target mask: ', tgt.shape)
                 padding_mask = create_padding_mask(tgt).to(device).type(indication_prompt.dtype)
                 #causal_mask1 = create_causal_masks(inputs)
@@ -186,17 +222,26 @@ def evaluate(model, accelerator, eval_loader, custom_loss, bce_loss):
                 
                 memory = encoded_images * att_wts # [batch_size, seq_len, d_model]
                 #memory_mask = None
-                
+                if torch.any(torch.isinf(memory)) or torch.any(torch.isnan(memory)):
+                    print("Memory is affected contains nan values..")
+                    
+                if torch.any(torch.isnan(encoder_pad_mask)) or torch.any(torch.isnan(padding_mask)) or torch.any(torch.isnan(tgt_mask)):
+                    print("Padding mask contains nan values...")
                 # Compute attention for visual_features, encoded_prompt
                 #memory = model.prompt_attention(memory, indication_prompt, key_padding_mask=mem_mask, residual_connection=True)
                 # print(memory.shape, indication_prompt.shape, tgt.shape, prev_hidden.shape)
                 # print(encoder_pad_mask.shape)
-                output = model.decoder(tgt, prev_hidden, (indication_prompt,memory),tgt_key_padding_mask=padding_mask,
+                output = model.decoder(tgt, prev_hidden, (indication_prompt, memory), tgt_key_padding_mask= padding_mask,
                                     memory_key_padding_mask=encoder_pad_mask, tgt_mask=tgt_mask,
-                                        tgt_is_causal=False)  # [batch_size, seq_len - 1, d_model]
+                                        tgt_is_causal=False)  # [batch_size, seq_len - 1, d_model] 
                 
-                #print("Output shape: ", output.shape)
-            
+                # if torch.any(torch.isinf(output)) or torch.any(torch.isnan(output)):
+                #     print("Output is affected...")
+                    
+                # print("output shape: ", output.shape, reports[:, i, 1:].shape)
+                # print("stop prob shape: ", pred_stop_probs.shape, true_stop_probs[:, 0].shape)
+                
+                #loss += custom_loss(true_stop_probs[:,i].type(indication_prompt.dtype), reports[:, i, 1:], pred_stop_probs,  output, eval=True)  # Ignore <sos> token
                 stop_loss, sparse_loss = custom_loss(true_stop_probs[:,i].type(indication_prompt.dtype), reports[:, i, 1:],pred_stop_probs,  output, eval=True)  # Ignore <sos> token
                 
                 eval_stop_losses.append(accelerator.gather(stop_loss).detach().cpu())
@@ -356,7 +401,7 @@ def train(cfg: DictConfig):
             # encoded_images = encoded_images.to(device)
             # reports = reports.to(device)
             # true_stop_probs = true_stop_probs.to(device)
-
+            print(f"Step: {step} \n\n")
             loss = 0            
             n_sentences  = reports.shape[1]
             # print('Length of sentences: ', n_sentences)
