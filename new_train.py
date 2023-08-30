cc45dd0946d4706293fc3e587449ab7d84e93f42
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-08-30 18:31:16 +0100

    fix bugs

--- new_train.py
+++ new_train.py
@@ -178,7 +178,7 @@ def evaluate(model, accelerator, eval_loader, custom_loss): #, bce_loss
                     print("Initial Prev hidden, hn, cn is nan")
                     
             for i in range(n_sentences):
-                print(f"This loop is for the number {i} sentence.")
+                ##print(f"This loop is for the number {i} sentence.")
                 
                 for name , each in model.named_parameters():
                     if torch.any(torch.isnan(each)):
