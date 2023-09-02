ba6377aab7a490c814b52e12bb5e572a141f0f51
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-09-02 21:16:35 +0100

    uncomment

--- new_train.py
+++ new_train.py
@@ -313,7 +313,7 @@ def train(cfg: DictConfig):
     )
 
     deepspeed_plugin = DeepSpeedPlugin(zero_stage=2, gradient_accumulation_steps=cfg.training.gradient_accumulation_steps, gradient_clipping=1.0)
-    accelerator = Accelerator(  deepspeed_plugin =deepspeed_plugin) #,  mixed_precision='fp16',
+    accelerator = Accelerator(mixed_precision='fp16',  deepspeed_plugin =deepspeed_plugin) #,  
     
     accelerator.wait_for_everyone()
     device= accelerator.device
