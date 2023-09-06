47e0dde1e6e0952e82feef65975e42639ae6351b
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-09-06 01:03:25 +0100

    uncomment

--- dataset.py
+++ dataset.py
@@ -203,7 +203,7 @@ class ChestXrayDataSet2(Dataset):
         else:
             # with open(caption_json, 'r') as f:
             #     self.data  = json.load(f)
-            self.data = pd.read_json(caption_json).numpy()
+            self.data = pd.read_json(caption_json).values()
             #print(self.data.columns)
         #self.file_names, self.labels = self.__load_label_list(file_list)
         if use_tokenizer_fast:
