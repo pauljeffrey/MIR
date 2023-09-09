ea60cb2a9618393c75d521b0c5e7947112016480
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-09-09 23:10:18 +0100

    fix bugs

--- new_train.py
+++ new_train.py
@@ -434,12 +434,12 @@ def train(cfg: DictConfig):
     #del data
     # Create train_loader and eval_loader here
     #if cfg.tokenizer.name is not None:
-    train_loader = get_loader2(cfg.dataset.train.image_dir,"/kaggle/working/train.json", # cfg.dataset.train.caption_json, 
+    train_loader = get_loader2(cfg.dataset.train.image_dir,cfg.dataset.train.caption_json, 
             tokenizer_name = cfg.tokenizer.name, transform= transform, batch_size = cfg.training.train_batch_size, s_max= cfg.dataset.tokens.s_max,
             n_max=cfg.dataset.tokens.n_max, encoder_n_max=cfg.dataset.tokens.encoder_n_max, shuffle=cfg.training.shuffle, use_tokenizer_fast=cfg.tokenizer.use_fast,
             collate_fn=collate_fn2, sampler= sampler)
     
-    eval_loader = get_loader2(cfg.dataset.eval.image_dir, "/kaggle/working/validation.json", #cfg.dataset.eval.caption_json, 
+    eval_loader = get_loader2(cfg.dataset.eval.image_dir, cfg.dataset.eval.caption_json, 
             tokenizer_name = cfg.tokenizer.name, transform= transform, batch_size = cfg.training.eval_batch_size, s_max= cfg.dataset.tokens.s_max,
             n_max=cfg.dataset.tokens.n_max, encoder_n_max=cfg.dataset.tokens.encoder_n_max, shuffle=cfg.training.shuffle, use_tokenizer_fast=cfg.tokenizer.use_fast, collate_fn=collate_fn2)
     # else:
