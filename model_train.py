3e35c58f2f30c0b031a7225e5bfae2fcd98de233
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-09-22 11:07:26 +0100

    fix bugs

--- model_train.py
+++ model_train.py
@@ -198,8 +198,8 @@ def train(cfg: DictConfig):
             # indication_prompt = torch.tensor(indication_prompt).type(torch.LongTensor).to(device)
             # true_stop_probs = torch.tensor(true_stop_probs).type(torch.LongTensor).to(device)
             # reports = torch.tensor(reports).type(torch.LongTensor).to(device)
-            
-            print(encoded_images.shape, indication_prompt.shape, true_stop_probs.shape, reports.shape)
+            if step % 500 == 0:
+                print(encoded_images.shape, indication_prompt.shape, true_stop_probs.shape, reports.shape)
             
             loss = 0            
             # n_sentences  = reports.shape[1]
