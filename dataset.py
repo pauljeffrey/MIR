0a564769fb21d65d6aa0d0957b77b94e560437c3
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-09-26 13:07:47 +0100

    changed loss function

--- dataset.py
+++ dataset.py
@@ -586,7 +586,7 @@ if __name__ == '__main__':
     #     replacement=False
     # )
     
-    cfg = OmegaConf.load("/kaggle/working/MIR/conf/config.yaml") #
+    cfg = OmegaConf.load("/content/sample_data/MIR/conf/config.yaml") #
     train_loader = get_loader2(cfg.dataset.train.image_dir, cfg.dataset.train.caption_json, 
             tokenizer_name = cfg.tokenizer.name, transform= transform, batch_size = cfg.training.train_batch_size, s_max= cfg.dataset.tokens.s_max,
             n_max=cfg.dataset.tokens.n_max, encoder_n_max=cfg.dataset.tokens.encoder_n_max, shuffle=cfg.training.shuffle, use_tokenizer_fast=cfg.tokenizer.use_fast, 
